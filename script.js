08a47aa38325013142624dc0e06001bc761db9fc
vcastellar <vic.sebastia@gmail.com>
2026-04-07 21:59:15 +0200

    Update question text for clarity

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const questions = [
   {
-    text: 'Posas encantado cuando alguien propone una foto de grupo, o huyes como si cada flash te quitara un año de vida?',
+    text: 'Posas encantado cuando alguien propone una foto de grupo consigues evitar huir como si cada flash te quitara un año de vida?',
     weight: 5,
   },
   {
